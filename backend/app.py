501da40d77aae9c7b0a790bb29d7cfe23b4f2cd3
hess.rafael95@gmail.com <hess.rafael95@gmail.com>
2023-12-04 21:18:19 -0300

    Removendo imports inuteis

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,6 @@
 from flask_openapi3 import OpenAPI, Info, Tag
 from flask import redirect
-from urllib.parse import unquote
 
-from sqlalchemy.exc import IntegrityError
-
-# from model import Session, Passageiro , Modelo
 from model import Passageiro, Modelo
 from schemas import *
 from flask_cors import CORS
